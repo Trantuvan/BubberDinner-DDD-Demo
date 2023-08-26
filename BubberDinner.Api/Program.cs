2caf2fbebde18c42d4d07506933f56e0db40561f
trantuvan <trantuvan.kan@gmail.com>
2023-08-26 13:22:12 +0700

    Register DI for auth service

--- BubberDinner.Api/Program.cs
+++ BubberDinner.Api/Program.cs
@@ -1,11 +1,14 @@
+using BubberDinner.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
     builder.Services.AddControllers();
 };
 
 var app = builder.Build();
 {
-    app.UseHttpsRedirection();
+    // app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
 }
