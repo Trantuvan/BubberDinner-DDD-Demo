2ae2fa88e53248527010aa111fcf6adc54243589
trantuvan <trantuvan.kan@gmail.com>
2023-09-09 18:39:50 +0700

    map section name JwtSettings from appsettings.development.json

--- BubberDinner.Infrastructure/DependencyInjection.cs
+++ BubberDinner.Infrastructure/DependencyInjection.cs
@@ -3,14 +3,18 @@ using BubberDinner.Application.Common.Interfaces.Services;
 using BubberDinner.Infrastructure.Authentication;
 using BubberDinner.Infrastructure.Services;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BubberDinner.Infrastructure;
 
 public static class DependencyInjection
 {
-  public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+  public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+                                                     ConfigurationManager configuration)
   {
+    services.Configure<JwtSettings>(configuration.GetSection(nameof(JwtSettings)));
+
     services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>()
             .AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
