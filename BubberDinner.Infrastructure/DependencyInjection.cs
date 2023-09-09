478d9cc67a6ce91ae5149f6a14af62d02bec9703
trantuvan <trantuvan.kan@gmail.com>
2023-09-09 16:17:00 +0700

    register JwtTokenGenerator & DateTimeProvider as Singleton

--- BubberDinner.Infrastructure/DependencyInjection.cs
+++ BubberDinner.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BubberDinner.Application.Common.Interfaces.Authentication;
+using BubberDinner.Application.Common.Interfaces.Services;
+using BubberDinner.Infrastructure.Authentication;
+using BubberDinner.Infrastructure.Services;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BubberDinner.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
-    {
-        return services;
-    }
+  public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+  {
+    services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>()
+            .AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+    return services;
+  }
 }
