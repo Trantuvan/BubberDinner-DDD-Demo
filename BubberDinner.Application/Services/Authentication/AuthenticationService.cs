29d0b31b60054dd1a65c675fed30a1e8a8a345ec
trantuvan <trantuvan.kan@gmail.com>
2023-09-09 16:12:13 +0700

    Return Jwt token when register new user
    + Check if user exist
    + Create new userID
    + generate JWT token

--- BubberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BubberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,16 @@
+using BubberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BubberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(Guid.NewGuid(),
@@ -13,10 +22,18 @@ public class AuthenticationService : IAuthenticationService
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(),
+        //* Check if the user already exists
+
+        //* Create new user (generate unique ID GUID)
+        Guid userId = Guid.NewGuid();
+
+        //* Generate JWT token
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+        return new AuthenticationResult(userId,
                                         firstName,
                                         lastName,
                                         email,
-                                        "token");
+                                        token);
     }
 }
