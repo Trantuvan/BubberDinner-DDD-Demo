c65adb95b6abbe6cf3ddd46430a2e14894448fff
trantuvan <trantuvan.kan@gmail.com>
2023-09-24 20:26:03 +0700

    Clean Up parameters with User Domain

--- BubberDinner.Api/Controllers/AuthenticationController.cs
+++ BubberDinner.Api/Controllers/AuthenticationController.cs
@@ -9,42 +9,42 @@ namespace BubberDinner.Api.Controllers;
 [Route("auth")]
 public class AuthenticationController : ControllerBase
 {
-    private readonly IAuthenticationService _authenticationService;
+  private readonly IAuthenticationService _authenticationService;
 
-    public AuthenticationController(IAuthenticationService authenticationService)
-    {
-        _authenticationService = authenticationService;
-    }
+  public AuthenticationController(IAuthenticationService authenticationService)
+  {
+    _authenticationService = authenticationService;
+  }
 
-    [HttpPost("register")]
-    public IActionResult Register(RegisterRequest request)
-    {
-        var authResult = _authenticationService.Register(request.FirstName,
-                                                         request.LastName,
-                                                         request.Email,
-                                                         request.Password);
+  [HttpPost("register")]
+  public IActionResult Register(RegisterRequest request)
+  {
+    var authResult = _authenticationService.Register(request.FirstName,
+                                                     request.LastName,
+                                                     request.Email,
+                                                     request.Password);
 
-        // * Map authResult to API contract response
-        var response = new AuthenticationResponse(authResult.Id,
-                                                  authResult.FirstName,
-                                                  authResult.LastName,
-                                                  authResult.Email,
-                                                  authResult.Token);
-        return Ok(response);
-    }
+    // * Map authResult to API contract response
+    var response = new AuthenticationResponse(authResult.User.Id,
+                                              authResult.User.FirstName,
+                                              authResult.User.LastName,
+                                              authResult.User.Email,
+                                              authResult.Token);
+    return Ok(response);
+  }
 
-    [HttpPost("login")]
-    public IActionResult Login(LoginRequest request)
-    {
-        var authResult = _authenticationService.Login(request.Email,
-                                                      request.Password);
+  [HttpPost("login")]
+  public IActionResult Login(LoginRequest request)
+  {
+    var authResult = _authenticationService.Login(request.Email,
+                                                  request.Password);
 
-        // * Map authResult to API contract response
-        var response = new AuthenticationResponse(authResult.Id,
-                                                  authResult.FirstName,
-                                                  authResult.LastName,
-                                                  authResult.Email,
-                                                  authResult.Token);
-        return Ok(response);
-    }
+    // * Map authResult to API contract response
+    var response = new AuthenticationResponse(authResult.User.Id,
+                                              authResult.User.FirstName,
+                                              authResult.User.LastName,
+                                              authResult.User.Email,
+                                              authResult.Token);
+    return Ok(response);
+  }
 }
--- BubberDinner.Application/Services/Authentication/AuthenticationResult.cs
+++ BubberDinner.Application/Services/Authentication/AuthenticationResult.cs
@@ -1,7 +1,5 @@
+using BubberDinner.Domain.Entities;
+
 namespace BubberDinner.Application.Services.Authentication;
 
-public record AuthenticationResult(Guid Id,
-                                     string FirstName,
-                                     string LastName,
-                                     string Email,
-                                     string Token);
+public record AuthenticationResult(User User, string Token);
